b0aa1213b846d60e7bf5516beb589667137da491
elp87 <elp1987@gmail.com>
2014-02-18 19:01:22 +0400

    Initial commit

---
+++ Battery/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Phone.Controls;
+using BatteryAPI = Windows.Phone.Devices.Power;
+
+namespace Battery
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+
+        private readonly BatteryAPI.Battery _battery;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _battery = BatteryAPI.Battery.GetDefault();
+            _battery.RemainingChargePercentChanged += OnRemainingChargePercentChanged;
+
+            UpdateUI();
+        }
+
+        private void OnRemainingChargePercentChanged(object sender, object e)
+        {
+            BatteryChargeTextBlock.Dispatcher.BeginInvoke(delegate()
+            {
+                UpdateUI();
+            });
+        }
+
+        private void UpdateUI()
+        {
+            BatteryChargeTextBlock.Text = _battery.RemainingChargePercent + "% ( осталось - " +
+                _battery.RemainingDischargeTime.Hours + "ч." + _battery.RemainingDischargeTime.Minutes + ")";
+        }
+    }
+}
